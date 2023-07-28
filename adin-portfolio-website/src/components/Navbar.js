05f1365e10e8836037566b7cb50995e6f9ebd51a
Wittiicus <adrixdewit@gmail.com>
2023-07-28 00:21:21 -0400

    Added basic nav bar

---
+++ adin-portfolio-website/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import '../index.css';
+
+function Navbar() {
+    return(
+    <div className = "nav-bar">
+        <ul className = "nav-list">
+            <li>
+                About Me
+            </li>
+            <li>
+                Projects
+            </li>
+            <li>
+                Blog
+            </li>
+        </ul>
+    </div>);
+}
+
+export default Navbar;
